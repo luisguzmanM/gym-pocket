8d03da622db7d094c515d75dab33f3ed013c88b9
Luis Guzman <guzmanluis.lg@gmail.com>
2024-07-14 11:49:41 -0600

    change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,12 @@
-import { ENV } from "./variables";
-
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: ENV.API_KEY,
-    authDomain: ENV.AUTH_DOMAIN,
-    projectId: ENV.PROJECT_ID,
-    storageBucket: ENV.STORAGE_BUCKET,
-    messagingSenderId: ENV.MESSAGING_SENDER_ID,
-    appId: ENV.APP_ID,
-    measurementId: ENV.MEASUREMENT_ID
+    apiKey: "AIzaSyD1mRSzTIg02Es5KOhpoTy5BHamquFcqnU",
+    authDomain: "gym-connect-7d552.firebaseapp.com",
+    projectId: "gym-connect-7d552",
+    storageBucket: "gym-connect-7d552.appspot.com",
+    messagingSenderId: "786796042453",
+    appId: "1:786796042453:web:b3f03b9f3d385557ff2a5c",
+    measurementId: "G-560X1WGQPT"
   }
 };
