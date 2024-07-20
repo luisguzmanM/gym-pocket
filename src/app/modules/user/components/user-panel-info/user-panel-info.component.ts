9135713fbab068516cc5695c9d880acbd2bb9709
Luis Guzman <guzmanluis.lg@gmail.com>
2024-07-20 14:03:24 -0500

    Add status point

--- src/app/modules/user/components/user-panel-info/user-panel-info.component.ts
+++ src/app/modules/user/components/user-panel-info/user-panel-info.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../../models/user.model';
-import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-panel-info',
@@ -10,26 +9,9 @@ import { UserService } from '../../services/user.service';
 export class UserPanelInfoComponent  implements OnInit {
 
   @Input() data!: User;
-  toggleState!: boolean;
 
-  constructor(
-    private _userSvc: UserService
-  ) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.toggleState = this.data.isPaymentDue;
-  }
-
-  async changePaymentState(){
-    let { customerID, isPaymentDue } = this.data;
-    isPaymentDue === true ? isPaymentDue = false : isPaymentDue = true;
-    try {
-      if('id' in this.data){
-        // await this._userSvc.updateUserPaymentStatus(id, isPaymentDue);
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
+  ngOnInit(): void {}
 
 }
