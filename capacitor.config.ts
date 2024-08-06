6ae7548d1d4d0c743b0d28a6117d60856cde13f6
Luis Guzman <guzmanluis.lg@gmail.com>
2024-08-06 00:56:05 -0500

    firebase credentials gitignore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'gym-connect',
+  appId: 'com.gympucket.app',
+  appName: 'Gym Pucket',
   webDir: 'www'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,14 @@
+import { firebaseConfig } from './firebase';
+
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyD1mRSzTIg02Es5KOhpoTy5BHamquFcqnU",
-    authDomain: "gym-connect-7d552.firebaseapp.com",
-    projectId: "gym-connect-7d552",
-    storageBucket: "gym-connect-7d552.appspot.com",
-    messagingSenderId: "786796042453",
-    appId: "1:786796042453:web:b3f03b9f3d385557ff2a5c",
-    measurementId: "G-560X1WGQPT"
+    apiKey: firebaseConfig.apiKey,
+    authDomain: firebaseConfig.authDomain,
+    projectId:firebaseConfig.projectId,
+    storageBucket: firebaseConfig.storageBucket,
+    messagingSenderId: firebaseConfig.messagingSenderId,
+    appId: firebaseConfig.appId,
+    measurementId: firebaseConfig.measurementId
   }
 };
